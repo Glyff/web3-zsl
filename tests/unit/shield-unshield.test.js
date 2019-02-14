f318c2aadb7af63ddf6258af79a083b45967bbb4
Vedmant <vedmant@gmail.com>
2019-02-14 11:29:13 +0700

    WIP on tests

--- tests/unit/shield-unshield.test.js
+++ tests/unit/shield-unshield.test.js
@@ -23,18 +23,18 @@ describe('shield unshield', () => {
     })
   })
 
-  it('should have test address with sufficient balances', async () => {
+  it('should have test address with sufficient balances', () => {
     return co(function *() {
-      debug('Balance is: ' + (yield web3.eth.getBalance(config.address)))
       const glyBalance = new BN(yield web3.eth.getBalance(config.address))
       const glxBalance = new BN(yield tokenContract.methods.balanceOf(config.address).call())
-
+      debug('Balance is: ' + glyBalance.toString())
+      debug('Token balance is: ' + glxBalance.toString())
       expect(glyBalance.gt(web3.utils.toWei('1', 'ether'))).toBe(true)
       expect(glxBalance.gt(web3.utils.toWei('1', 'szabo'))).toBe(true)
     })
   })
 
-  it('should shield successfully', async (done) => {
+  it('should shield successfully', (done) => {
     co(function *() {
       yield web3.eth.personal.unlockAccount(config.address, config.password, 9999)
 
@@ -45,17 +45,16 @@ describe('shield unshield', () => {
       const result = yield web3.zsl.createShielding(rho, tracker.a_pk, value.toNumber())
       debug('Generating finished')
 
-      const txHash = yield new Promise((resolve, reject) => {
-        tokenContract.methods.shield(result.proof, result.send_nf, result.cm, value)
-          .send({from: config.address, gas: 200000}, (err, hash) => {
-            debug('Transaction sent', {err, hash})
-            if (err) reject(err)
-            resolve(hash)
-          })
+      const hash = yield new Promise((resolve, reject) => {
+        tokenContract.methods.shield(result.proof, result.send_nf, result.cm, value.toNumber())
+          .send({from: config.address, gas: 200000})
+          .once('transactionHash', (hash) => resolve(hash))
+          .catch(err => reject(err))
       })
 
+      debug('Shield transaction sent, hash: ' + hash)
+
       note = {
-        txHash,
         rho,
         value,
         uuid: web3.utils.toHex(web3.utils.sha3(result.cm, {encoding: 'hex'})),
@@ -80,7 +79,7 @@ describe('shield unshield', () => {
     })
   }, 300000)
 
-  it('should unshield successfully', async (done) => {
+  it('should unshield successfully', (done) => {
     co(function *() {
       debug('Generating proof for unshielding')
       const cm = yield web3.zsl.getCommitment(note.rho, tracker.a_pk, note.value.toNumber())
@@ -88,32 +87,33 @@ describe('shield unshield', () => {
       const treeIndex = parseInt(witnesses[0])
       const authPath = witnesses[1]
       const root = yield tokenContract.methods.root().call()
-      debug('CreateUnshielding', {rho: note.rho, a_sk: tracker.a_sk, value: note.value.toNumber(), treeIndex, authPath})
+      debug('CreateUnshielding', {rho: note.rho, a_sk: tracker.a_sk, value: note.value.toNumber(), treeIndex})
       const unsh = yield web3.zsl.createUnshielding(note.rho, tracker.a_sk, note.value.toNumber(), treeIndex, authPath)
       debug('Generating proof finished')
 
-      yield new Promise((resolve, reject) => {
-        tokenContract.methods.unshield(unsh.proof, unsh.send_nf, cm, root, note.value)
-          .send({from: config.address, gas: appConfig.unshieldGas.toNumber()}, (err, hash) => {
-            debug('Unshielding transaction sent', {err, hash})
-            if (err) reject(err)
-            resolve(hash)
-          })
+      const hash = yield new Promise((resolve, reject) => {
+        tokenContract.methods.unshield(unsh.proof, unsh.send_nf, cm, root, note.value.toNumber())
+          .send({from: config.address, gas: 200000})
+          .once('transactionHash', (hash) => resolve(hash))
+          .on('error', err => reject(err))
+          .then(receipt => debug({receipt}))
+          .catch(err => reject(err))
       })
+      debug('Unshield transaction sent, hash: ' + hash)
 
       debug('Waiting for comfirmation')
       tokenContract.events.LogUnshielding({}, (err, event) => {
-        debug('Recieved LogUnshielding event')
+        debug('Recieved LogUnshielding event', event.returnValues)
         debug({err, event})
 
         expect(err).toBeFalsy()
         expect(event.returnValues.from).toBe(config.address)
-        expect(event.returnValues['2']).toBe(note.uuid)
+        expect(event.returnValues.uuid).toBe(note.uuid)
         done()
       })
 
       expect.assertions(3)
     })
-  }, 600000)
+  }, 400000)
 
 })
--- tests/unit/verify-unshield.test.js
+++ tests/unit/verify-unshield.test.js
@@ -9,6 +9,7 @@ const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://' + config.host
 describe('verify unshield', () => {
   let tokenContract
   let tracker
+  let note
 
   beforeAll(() => {
     return co(function* () {
@@ -25,18 +26,21 @@ describe('verify unshield', () => {
   it('should verify that unshieling is correct', () => {
     return co(function *() {
       debug('Balance: ' + (yield web3.eth.getBalance(config.address)))
+      debug('Tokens balance: ' + (yield tokenContract.methods.balanceOf(config.address).call()))
 
       const rho = yield web3.zsl.getRandomness()
       const value = new BN(web3.utils.toWei('1', 'szabo'))
+      debug({rho, pk: tracker.a_pk, value: value.toNumber()})
       const cm = yield web3.zsl.getCommitment(rho, tracker.a_pk, value.toNumber())
-      debug({cm})
+      debug('Got commitment', {cm})
 
       const witnesses = yield tokenContract.methods.getWitness(cm).call()
+      debug('Got witnesses')
       debug({witnesses})
       const treeIndex = parseInt(witnesses[0])
       const authPath = witnesses[1]
 
-      debug({rho, sk: tracker.a_sk, value: value.toNumber(), treeIndex, authPath})
+      debug({rho, sk: tracker.a_sk, value: value.toNumber(), treeIndex})
 
       const unsh = yield web3.zsl.createUnshielding(rho, tracker.a_sk, value.toNumber(), treeIndex, authPath)
 
